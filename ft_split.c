c40e1160331228d871d64fd0abd2ccbeec61f7bc
kimitriy <59221010+kimitriy@users.noreply.github.com>
2021-06-11 23:25:01 +0300

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -12,31 +12,65 @@
 
 #include "libft.h"
 
+int	words_counter(const char *str, char dlmtr)
+{
+	int		wn; //words number
+	int		ndf; //non dlmtr flag
+	int		i;
+
+	i = 0;
+	wn = 0;
+	ndf = 0;
+	while (str[i])
+	{
+		if (str[i] != dlmtr && ndf == 0 && str[i + 1] != '\0')
+			ndf = 1;
+		else if (str[i] != dlmtr && ndf == 0 && str[i + 1] == '\0')
+			wn++;
+		else if ((str[i] == dlmtr && ndf == 1) || (str[i] != dlmtr && str[i + 1] == '\0'))
+		{
+			wn++;
+			ndf = 0;
+		}
+		i++;
+	}
+	return (wn);
+}
+
+void	parse_and_write_to_arr(char **arr, const char *str, char dlmtr, int wn)
+{
+	int				i;
+	int				wi; //word indx
+	size_t			len; //length number
+	unsigned int	strt; //start indx
+
+	i = -1;
+	wi = 0;
+	strt = 0;
+	while (wi < wn && str[++i])
+	{
+		if (str[i] != dlmtr)
+		{
+			strt = i;
+			len = 0;
+			while (str[i] != dlmtr && str[i] != '\0')
+			{
+				len++;
+				i++;
+			}	
+			arr[wi] = ft_substr(str, strt, len);
+			wi++;
+		}
+	}
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
-	size_t	wrdn;
-	size_t	i;
-	size_t	strt;
+	int		wn; //words number
 
-	if (!s)
-		return (NULL);
-	i = -1;
-	wrdn = 0;
-	while (s[++i])
-		if ((s[i] == c && (s[i + 1] != c &&
-		s[i + 1] != 0)) || (s[i] != c && s[i + 1] == 0))
-			wrdn++;
-	if (!(arr = (char**)malloc((wrdn + 1) * sizeof(char*))))
-		return (NULL);
-	i = -1;
-	wrdn = 0;
-	strt = 0;
-	while (s[++i])
-		if (s[i] == c && s[i + 1] != c)
-			strt = i + 1;
-		else if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
-			arr[wrdn++] = ft_substr(s, strt, i - strt + 1);
-	arr[wrdn] = NULL;
+	wn = words_counter(s, c);
+	arr = (char **)ft_calloc(wn + 1, sizeof(char *));
+	parse_and_write_to_arr(arr, s, c, wn);
 	return (arr);
 }
